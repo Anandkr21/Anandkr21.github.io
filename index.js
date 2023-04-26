ebe8cdecd72e992d3ab904c184d7c4d48a0c9ac9
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-04-26 14:52:37 +0530

    resume updated

--- index.js
+++ index.js
@@ -1,103 +1,103 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
+  $(window).scroll(function () {
 
-      // sticky navbar on scroll script
-      if (this.scrollY > 20) {
-        $("#nav-menu").addClass("sticky");
-      } else {
-        $("#nav-menu").removeClass("sticky");
-      }
-  
-      // scroll-up button show/hide script
-      if (this.scrollY > 500) {
-        $(".scroll-up-btn").addClass("show");
-      } else {
-        $(".scroll-up-btn").removeClass("show");
-      }
-    });
-  
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $("#nav-menu").addClass("sticky");
+    } else {
+      $("#nav-menu").removeClass("sticky");
+    }
 
-    // slide-up script
-    $(".scroll-up-btn").click(function () {
-      $("html").animate({ scrollTop: 0 });
-      // removing smooth scroll on slide-up button click
-      $("html").css("scrollBehavior", "auto");
-    });
-  
-
-    $("#nav-menu .menu li a").click(function () {
-      // applying again smooth scroll on menu items click
-      $("html").css("scrollBehavior", "smooth");
-    });
-  
-
-    // toggle menu/navbar script
-    $(".menu-btn").click(function () {
-      $("#nav-menu .menu").toggleClass("active");
-      $(".menu-btn i").toggleClass("active");
-    });
-  
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-      strings: ["Back-End Developer", "Daydreamer"],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true,
-    });
-  
-
-    var typed = new Typed(".typing-2", {
-      strings: ["Back-End Developer", "Daydreamer"],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true,
-    });
-  
-
-    // owl carousel script
-    $(".carousel").owlCarousel({
-      margin: 20,
-      loop: true,
-      autoplay: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive: {
-        0: {
-          items: 1,
-          nav: false,
-        },
-        600: {
-          items: 2,
-          nav: false,
-        },
-        1000: {
-          items: 3,
-          nav: false,
-        },
-      },
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
   });
-  
-  
-  let drive = document
-    .querySelector(".Drivegoogle")
-    .addEventListener("click", openDrive);
-  
-  function openDrive() {
-    window.open(
-      (href =
-        "https://drive.google.com/file/d/1iUGkZWUNxb4lR_VnnnASF-rPdIesBQBS/view?usp=share_link")
-    );
-  }
-  
-  let drive2 = document
+
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+
+  $("#nav-menu .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $("#nav-menu .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Back-End Developer", "Daydreamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Back-End Developer", "Daydreamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+
+let drive = document
+  .querySelector(".Drivegoogle")
+  .addEventListener("click", openDrive);
+
+function openDrive() {
+  window.open(
+    (href =
+      "https://drive.google.com/file/d/1Xr0EtXHM0ZXSNPtNY1F7g0Pn_p7t579D/view?usp=share_link")
+  );
+}
+
+let drive2 = document
   .querySelector(".googleDrive")
   .addEventListener("click", openDrive2);
 
-  function openDrive2(){
-    window.open(
-      (href =
-        "https://drive.google.com/file/d/1iUGkZWUNxb4lR_VnnnASF-rPdIesBQBS/view?usp=share_link")
-    );
-  }
+function openDrive2() {
+  window.open(
+    (href =
+      "https://drive.google.com/file/d/1Xr0EtXHM0ZXSNPtNY1F7g0Pn_p7t579D/view?usp=share_link")
+  );
+}
